ddaef0f92a3459e2d0ee58a5192d2de9daa1e9e6
KARTHIK CHARY <34944541+karthikchary@users.noreply.github.com>
2021-11-10 19:43:28 +0530

    Revert "updated account and added file"

--- account.py
+++ account.py
@@ -7,5 +7,4 @@ class account:
 
 if __name__ == '__main__':
     accVerify = account()
-    print('Acoount pyhton file')
     print('The password length is fair' + str(accVerify.check_password_length('offtoschool')))
